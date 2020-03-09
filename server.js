631b79dc2628121735767001c8bf64a81e32a4fb
Daniel <daniil.elshin.ext@lafargeholcim.com>
2020-03-09 10:07:44 +0100

    Update server.js

--- server.js
+++ server.js
@@ -137,7 +137,7 @@ app.post('/infoPOST', function(req, res){
   };
   res.send(JSON.stringify(requestProps));
 });
-/*
+
 app.post('/senduserdata', function (req, res) {
   // try to initialize the db on every request if it's not already
   // initialized.
@@ -149,9 +149,9 @@ app.post('/senduserdata', function (req, res) {
     var col = db.collection('users');
     // Create a document with request IP and current time of request
     col.insert({user_mail: rBody.user_mail, date: Date.now()});
-    res.send(JSON.stringify{success: "saved"});
+    res.send(JSON.stringify({success: "saved"});
   } else {
-    res.send(JSON.stringify{error: "no database"});
+    res.send(JSON.stringify({error: "no database"});
   }
 });
 /**/
@@ -168,12 +168,12 @@ app.post('/getuserdata', function (req, res) {
     var query = { user_mail: req.body.user_mail };
     col.find(query).toArray(function(err, result) {
       if (err) throw err;
-      res.send(JSON.stringify{result: result});
+      res.send(JSON.stringify({result: result});
       db.close();
     });
     
   } else {
-    res.send(JSON.stringify{error: "no database"});
+    res.send(JSON.stringify({error: "no database"});
   }
 });/**/
 // error handling
