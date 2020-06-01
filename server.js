c38743ae2929aad406cb86dde324ab641d3a24f3
Daniel <daniil.elshin.ext@lafargeholcim.com>
2020-06-01 11:32:32 +0200

    test old

--- server.js
+++ server.js
@@ -3,20 +3,103 @@ var express = require('express'),
     app     = express(),
     morgan  = require('morgan');
     
-Object.assign=require('object-assign');
+Object.assign=require('object-assign')
 
 app.engine('html', require('ejs').renderFile);
-app.use(morgan('combined'));
+app.use(morgan('combined'))
 // Parse JSON bodies (as sent by API clients)
 app.use(express.json());
 
-var js_connection = require('./js/connection');
-console.log(js_connection.foo());
+var port = process.env.PORT || process.env.OPENSHIFT_NODEJS_PORT || 8080,
+    ip   = process.env.IP   || process.env.OPENSHIFT_NODEJS_IP || '0.0.0.0',
+    mongoURL = process.env.OPENSHIFT_MONGODB_DB_URL || process.env.MONGO_URL,
+    mongoURLLabel = "";
 
+if (mongoURL == null) {
+  console.log("DATABASE_SERVICE_NAME: " + process.env.DATABASE_SERVICE_NAME)
+  var mongoHost, mongoPort, mongoDatabase, mongoPassword, mongoUser;
+  // If using plane old env vars via service discovery
+  if (process.env.DATABASE_SERVICE_NAME) {
+    var mongoServiceName = process.env.DATABASE_SERVICE_NAME.toUpperCase();
+    mongoHost = process.env[mongoServiceName + '_SERVICE_HOST'];
+    mongoPort = process.env[mongoServiceName + '_SERVICE_PORT'];
+    mongoDatabase = process.env[mongoServiceName + '_DATABASE'];
+    mongoPassword = process.env[mongoServiceName + '_PASSWORD'];
+    mongoUser = process.env[mongoServiceName + '_USER'];
 
+  // If using env vars from secret from service binding  
+  } else if (process.env.database_name) {
+    mongoDatabase = process.env.database_name;
+    mongoPassword = process.env.password;
+    mongoUser = process.env.username;
+    var mongoUriParts = process.env.uri && process.env.uri.split("//");
+    if (mongoUriParts.length == 2) {
+      mongoUriParts = mongoUriParts[1].split(":");
+      if (mongoUriParts && mongoUriParts.length == 2) {
+        mongoHost = mongoUriParts[0];
+        mongoPort = mongoUriParts[1];
+      }
+    }
+  }
+
+  if (mongoHost && mongoPort && mongoDatabase) {
+    mongoURLLabel = mongoURL = 'mongodb://';
+    if (mongoUser && mongoPassword) {
+      mongoURL += mongoUser + ':' + mongoPassword + '@';
+    }
+    // Provide UI label that excludes user id and pw
+    mongoURLLabel += mongoHost + ':' + mongoPort + '/' + mongoDatabase;
+    mongoURL += mongoHost + ':' +  mongoPort + '/' + mongoDatabase;
+  }
+}
+var db = null,
+    dbDetails = new Object();
+
+var initDb = function(callback) {
+  console.log("initDB on: " + mongoURL);
+  if(mongoURL == null)
+    console.log("mongoURL is null");
+  if (mongoURL == null) return;
+
+  var mongodb = require('mongodb');
+  if(mongodb == null)
+    console.log("no mongodb");
+  if (mongodb == null) return;
+
+  mongodb.connect(mongoURL, function(err, conn) {
+    if (err) {
+      callback(err);
+      return;
+    }
+
+    db = conn;
+    dbDetails.databaseName = db.databaseName;
+    dbDetails.url = mongoURLLabel;
+    dbDetails.type = 'MongoDB';
+
+    console.log('Connected to MongoDB at: %s', mongoURL);
+  });
+};
 
 app.get('/', function (req, res) {
-  res.render('index.html', { pageCountMessage : null});
+  // try to initialize the db on every request if it's not already
+  // initialized.
+  if (!db) {
+    initDb(function(err){});
+  }
+  if (db) {
+    var col = db.collection('counts');
+    // Create a document with request IP and current time of request
+    col.insert({ip: req.ip, date: Date.now()});
+    col.count(function(err, count){
+      if (err) {
+        console.log('Error running count. Message:\n'+err);
+      }
+      res.render('index.html', { pageCountMessage : count, dbInfo: dbDetails });
+    });
+  } else {
+    res.render('index.html', { pageCountMessage : null});
+  }
 });
 
 app.get('/pagecount', function (req, res) {
@@ -61,61 +144,42 @@ app.post('/infoPOST', function(req, res){
 });
 
 app.post('/senduserdata', function (req, res) {
-
   // try to initialize the db on every request if it's not already
   // initialized.
   var rBody = req.body;
-  
-  var MongoClient = require('mongodb').MongoClient;
-
-  MongoClient.connect(js_connection.dbDetails.mongoURL, { useUnifiedTopology: true}, function(err, db) {
-    if (err) {
-      res.send(JSON.stringify({error: err}));
-      return;
-    }
-    var dbo = db.db(js_connection.dbDetails.databaseName);
-    var myobj = rBody.date = Date.now();
-    var myobj = rBody;
-    dbo.collection(js_connection.dbDetails.collection).insertOne(myobj, function(err, result) {
-      //console.log(err);
-      //console.log(result.insertedId);
-      if (err){
-        res.send(JSON.stringify({error: err}));
-      }
-      res.send(JSON.stringify({success: "saved", insertedId: result.insertedId}));
-      db.close();
-    });
-  });
+  if (!db) {
+    initDb(function(err){});
+  }
+  if (db) {
+    var col = db.collection('users');
+    // Create a document with request IP and current time of request
+    col.insert({user_mail: rBody.user_mail, date: Date.now()});
+    res.send(JSON.stringify({success: "saved"}));
+  } else {
+    res.send(JSON.stringify({error: "no database"}));
+  }
 });
 /**/
 app.post('/getuserdata', function (req, res) {
   // try to initialize the db on every request if it's not already
   // initialized.
   var rBody = req.body;
-
-  var MongoClient = require('mongodb').MongoClient;
-
-  MongoClient.connect(js_connection.dbDetails.mongoURL, { useUnifiedTopology: true}, function(err, db) {
-    if (err) {
-      res.send(JSON.stringify({error: err}));
-      return;
-    }
-    var dbo = db.db(js_connection.dbDetails.databaseName);
-    var myobj = rBody.date = Date.now();
-    var myobj = rBody;
-
-    var query = { user_mail: rBody.user_mail };
-    if(!rBody.user_mail)
-      query = {};
-    dbo.collection(js_connection.dbDetails.collection).find(query).toArray(function(err, result) {
-      if (err) {
-        res.send(JSON.stringify({error: err}));
-        return;
-      }
-      res.send(JSON.stringify({success: "exist data", data: result}));
+  if (!db) {
+    initDb(function(err){});
+  }
+  if (db) {
+    var col = db.collection('users');
+    // Create a document with request IP and current time of request
+    var query = { user_mail: req.body.user_mail };
+    col.find(query).toArray(function(err, result) {
+      if (err) throw err;
+      res.send(JSON.stringify({result: result}));
       db.close();
     });
-  });
+    
+  } else {
+    res.send(JSON.stringify({error: "no database"}));
+  }
 });/**/
 
 
@@ -125,11 +189,11 @@ app.use(function(err, req, res, next){
   res.status(500).send('Something bad happened: ' + err);
 });
 
-js_connection.initDb(function(err){
+initDb(function(err){
   console.log('Error connecting to Mongo. Message:\n'+err);
 });
 
-app.listen(js_connection.port, js_connection.ip);
-console.log('Server running on http://%s:%s', js_connection.ip, js_connection.port);
+app.listen(port, ip);
+console.log('Server running on http://%s:%s', ip, port);
 
 module.exports = app ;
